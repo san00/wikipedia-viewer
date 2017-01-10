102a3ce34ea480e3cc4bbb473fb2d73cd2cd39ec
san00 <c5d59d07@opayq.com>
2017-01-10 16:15:05 +0000

    Link to random wiki article
    Created button to link to random wiki article
    Also includes styling & positioning of page elements

--- wiki.js
+++ wiki.js
@@ -1,24 +1,24 @@
 function search() {
 
-  var searchWord = $("#searchbox").val();
-  var apiResult = "https://en.wikipedia.org/w/api.php?action=opensearch&search="
-  +searchWord + "&format=json";
+    var searchWord = $("#searchbox").val();
+    var apiResult = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
+        searchWord + "&format=json";
 
-  $.ajax({
-    url: apiResult,
-    dataType: "jsonp",
-    success: function(info){
-      //loop to return search results & display them on page
-      for (var i = 0; i < info[1].length; i++){
-       $("#searchresult").append("<a href=" +info[3][i]+"><h2>" + info[1][i]
-       + "</h2>" + info[2][i] + "</a>");
-     }
-    }
-  })
+    $.ajax({
+        url: apiResult,
+        dataType: "jsonp",
+        success: function(info) {
+            //loop to return search results & display them on page
+            for (var i = 0; i < info[1].length; i++) {
+                $("#searchresult").append("<a href=" + info[3][i] + "><h2>" + info[1][i] +
+                    "</h2>" + info[2][i] + "</a>");
+            }
+        }
+    })
 }
 //search using keypress or button click
-$("form").submit(function(press){
-  press.preventDefault();
-  search();
+$("form").submit(function(press) {
+    press.preventDefault();
+    search();
 });
- $("form").on('click', search);
+$("form").on('click', search);
