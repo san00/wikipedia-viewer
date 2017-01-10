c4417274c8956754f14c9b599ce5e30b01843a2a
san00 <c5d59d07@opayq.com>
2017-01-10 21:17:10 +0000

    Complete the project
    Updated JS to display only the current search result.
    Added title & other CSS styling.

--- wiki.js
+++ wiki.js
@@ -3,12 +3,11 @@ function search() {
     var searchWord = $("#searchbox").val();
     var apiResult = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
         searchWord + "&format=json";
-
+//return search results & display them
     $.ajax({
         url: apiResult,
         dataType: "jsonp",
         success: function(info) {
-            //loop to return search results & display them on page
             for (var i = 0; i < info[1].length; i++) {
                 $("#searchresult").append("<a href=" + info[3][i] + "><h2>" + info[1][i] +
                     "</h2>" + info[2][i] + "</a>");
@@ -16,9 +15,13 @@ function search() {
         }
     })
 }
-//search using keypress or button click
+//enter info using keypress or button click, also empty previously displayed results
 $("form").submit(function(press) {
     press.preventDefault();
     search();
+    $("#searchresult").empty();
 });
-$("form").on('click', search);
+$("form").click(function() {
+    search();
+    $("#searchresult").empty();
+})
